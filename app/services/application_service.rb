37bae4f1e333c50ae3435204dafc38a4f4a77198
Jake O'Donnell <jakeod99@gmail.com>
2023-10-17 22:36:59 -0400

    final structure tweaks plus lint test

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -4,6 +4,10 @@ class ApplicationService
     new(**).call
   end
 
+  def call
+    raise NotImplementedError
+  end
+
   private
 
   LOG_LEVELS = [:debug, :info, :warn, :error, :fatal, :unknown]
@@ -19,7 +23,7 @@ class ApplicationService
   end
 
   def failure(content = nil)
-    log(:info, "Failed".red)
+    log(:info, 'Failed'.red)
     ServiceResponse.new(status: :failure, content: content)
   end
 end
